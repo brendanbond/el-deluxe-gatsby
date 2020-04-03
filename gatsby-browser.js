7bb8e915cc2d4f5586236db5517d11a18dc038c4
brendanbond <brendanbond@gmail.com>
2020-04-03 17:41:39 -0500

    add intersection-observer polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,3 +7,11 @@
 // You can delete this file if you're not using it
 
 require("./src/fonts/fonts.css");
+
+export const onClientEntry = () => {
+  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+  if (!(`IntersectionObserver` in window)) {
+    import(`intersection-observer`);
+    console.log(`# IntersectionObserver is polyfilled!`);
+  }
+};
